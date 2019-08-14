972ccc09f8d26c4250d8a8971fb7cb7bc2e61f72
Abner López <abner.lopezchavez@gmail.com>
2019-08-13 18:52:15 -0600

    Added global README

---
+++ MiSeUser/ServiceRequest.asmx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace MiSeUser
+{
+    /// <summary>
+    /// Descripción breve de ServiceRequest
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServiceRequest : System.Web.Services.WebService
+    {
+        /**
+         * Método Request realiza la solicitud de servicio
+         * Parámetros:
+         * string user_id : usuario que solicita el servicio
+         * string user_phone : teléfono del usuario que solicita el servicio
+         * int user_pos_x : ubicación del usuario coordenada x
+         * int user_pos_y : ubicación del usuario coordenada y
+         * int user_pos_z : ubicación del usuario coordenada z
+         */
+
+        [WebMethod]
+        public string Request(string user_id, string user_phone, int user_pos_x, int user_pos_y, int user_pos_z)
+        {
+            return "Debe retornar si existe un piloto disponible";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# **UBER Simulator**
+
+Aplicación dónde se hace uso de microservicios orquestados mediante un ESB simulado.
+
+## Getting Started
+
+### Prerequisites
+
+- Visual Studio 2019 .NET Framework 4.7.2
+- Language: C#
+
+## Description
+
+El proyecto está dividido en tres microservicios:
+1. Microservicio Usuario
+2. Microservicio Pilotos
+3. Microservicio Rastreo de Ubicación
+
+Además de un ESB para orquestar los servicios mencionados anteriormente.
+
+## Author
+
+- Abner Lorenzo López Chávez - 201114705 - *Ingeniería en Sistemas* - Universidad de San Carlos
+
+## Acknowledgments
+
+- Web Services
