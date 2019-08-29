df0387c8a90e5e6deb56d3bd83d24eda6ed541c3
Abner López <abner.lopezchavez@gmail.com>
2019-08-28 18:46:08 -0600

    Added Driver Class - Get, Set Methods

---
+++ MiSeDriverFinder/Driver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiSeDriverFinder
+{
+    public class Driver
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public int Driver_pos_x { get; set; }
+        public int Driver_pos_y { get; set; }
+        public int Driver_pos_z { get; set; }
+
+        public Driver(string name, string number, int driver_pos_x, int driver_pos_y, int driver_pos_z)
+        {
+            Name = name;
+            Number = number;
+            Driver_pos_x = driver_pos_x;
+            Driver_pos_y = driver_pos_y;
+            Driver_pos_z = driver_pos_y;
+        }
+    }
+}
---
+++ MiSeDriverFinder/FindDriver.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="FindDriver.asmx.cs" Class="MiSeDriverFinder.FindDriver" %>
